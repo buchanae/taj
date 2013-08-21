e2dee377dfa4e824963208e66b554958f16471f0
Alex Buchanan <buchanae@gmail.com>
2013-08-20 17:28:41 -0700

    initial notes

---
+++ taj.py
@@ -0,0 +1,53 @@
+time
+
+board
+
+castles
+- position
+- area
+- defense
+
+knights
+- position
+- movement
+-- speed
+-- logic
+--- find nearest castle
+
+soliders
+- choosing which to attack
+-- attack other soliders (entities) in certain radius
+- possible damage
+- health
+
+lumberjacks
+- position
+- movement
+
+panners
+- position
+- movement
+
+miners
+- position
+- movement
+
+tankers
+- position
+- movement
+-- speed
+-- logic
+--- look for nearest water on land owned by clan or land owned by no clan
+
+trees
+- position
+- collectable
+
+water
+- position
+- collectable
+
+gold
+- position
+- collectable
+
